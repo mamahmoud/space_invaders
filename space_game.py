96f4c712bd3cf618e9dc4ffada413259bf0ffb12
Mohamed Ali <mohamed.ahmed1024@gmail.com>
2022-10-20 22:36:46 +0200

    nearer

--- space_game.py
+++ space_game.py
@@ -118,7 +118,7 @@ class Enemy:
                     player.score += 1
                     Bullet.max_bullets += 1
                     self.x_axis = random.randint(100, 700)
-                    self.y_axis = random.choice(INTIAL_ENEMY_POS_Y) + player.score // 2
+                    self.y_axis = random.choice(INTIAL_ENEMY_POS_Y) + player.score
                     break
 
     def draw(self):
